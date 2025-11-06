12de31b93c604a992d971cd0fe17f463932220da
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-11-06 10:05:26 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,5 +9,4 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
 }
