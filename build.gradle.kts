2931b45b8bb1be05101cdf506a04297f3656317f
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-11-06 10:06:33 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.application") version "8.13.0" apply false
     id("com.android.library") version "8.13.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    // id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.2.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
