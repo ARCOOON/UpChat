6a6e6e9d075d661bb00931c7fa79708a8dda89d5
Arcooon <leonv11@gmx.net>
2025-11-06 23:56:09 +0100

    Add KSP plugin and configure KSP2
    Adds the KSP Gradle plugin to the project and enables the KSP2 option to resolve a potential `UnknownPluginException`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.android.library") version "8.13.0" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.2.21" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
