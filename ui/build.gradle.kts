bee24380d3edb385d5b7e934e35706e817a6111f
Arcooon <leonv11@gmx.net>
2025-11-06 23:04:59 +0100

    Bump compose-bom and navigation-compose versions

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -57,18 +57,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
     implementation("androidx.activity:activity-compose:1.11.0")
 
-    implementation(platform("androidx.compose:compose-bom:2025.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.11.00"))
 
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-    implementation("androidx.navigation:navigation-compose:2.9.5")
+    implementation("androidx.navigation:navigation-compose:2.9.6")
     implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
     implementation("com.google.dagger:hilt-android:2.57.2")
     implementation("io.coil-kt:coil-compose:2.7.0")
     implementation("androidx.compose.material:material-icons-core:1.7.8")
-
-    androidTestImplementation(platform("androidx.compose:compose-bom:2025.10.01"))
 }
