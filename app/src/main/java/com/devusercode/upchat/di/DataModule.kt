20aec95fd7665b9fa3ae0b9c3cbc8c471584b20d
Leon <78073305+ARCOOON@users.noreply.github.com>
2025-11-07 12:14:01 +0100

    Update DataModule.kt

--- app/src/main/java/com/devusercode/upchat/di/DataModule.kt
+++ app/src/main/java/com/devusercode/upchat/di/DataModule.kt
@@ -1,14 +1,21 @@
 package com.devusercode.upchat.di
 
+import android.content.Context
+import androidx.room.Room
+import com.devusercode.core.domain.auth.repo.AuthRepository
 import com.devusercode.core.domain.chat.repo.ChatRepository
 import com.devusercode.core.domain.user.repo.UserRepository
+import com.devusercode.data.auth.FirebaseAuthRepository
 import com.devusercode.data.firebase.FirebaseChatRepository
 import com.devusercode.data.firebase.FirebaseUserRepository
+import com.devusercode.data.local.db.AppDatabase
+import com.devusercode.data.local.db.dao.ConversationDao
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -20,10 +27,29 @@ object DataModule {
     @Provides @Singleton fun provideDb(): FirebaseDatabase = FirebaseDatabase.getInstance()
 
     @Provides @Singleton
-    fun provideUserRepository(auth: FirebaseAuth, db: FirebaseDatabase): UserRepository =
-        FirebaseUserRepository(auth, db)
+    fun provideRoom(@ApplicationContext ctx: Context): AppDatabase =
+        Room.databaseBuilder(ctx, AppDatabase::class.java, "upchat.db")
+            .fallbackToDestructiveMigration(false)
+            .build()
+
+    @Provides fun provideConversationDao(db: AppDatabase): ConversationDao = db.conversationDao()
 
     @Provides @Singleton
-    fun provideChatRepository(db: FirebaseDatabase): ChatRepository =
-        FirebaseChatRepository(db)
+    fun provideUserRepository(
+        auth: FirebaseAuth,
+        db: FirebaseDatabase
+    ): UserRepository = FirebaseUserRepository(auth, db)
+
+    @Provides @Singleton
+    fun provideChatRepository(
+        db: FirebaseDatabase,
+        conversationDao: ConversationDao
+    ): ChatRepository = FirebaseChatRepository(db, conversationDao)
+
+    @Provides @Singleton
+    fun provideAuthRepository(
+        @ApplicationContext ctx: Context,
+        auth: FirebaseAuth,
+        db: FirebaseDatabase
+    ): AuthRepository = FirebaseAuthRepository(ctx, auth, db)
 }
