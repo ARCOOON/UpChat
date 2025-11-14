08d96d6dd386bc1ba4efc5fd12e0331388166483
Arcooon <leonv11@gmx.net>
2025-11-14 19:19:34 +0100

    chore: Update Gradle dependencies
    Updated Android Gradle Plugin, Android Library, and Room versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.jetbrains.kotlin.android") version "2.2.21" apply false
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
+    id("com.android.application") version "8.13.1" apply false
+    id("com.android.library") version "8.13.1" apply false
     id("org.jetbrains.kotlin.jvm") version "2.2.21" apply false
     id("org.jetbrains.kotlin.kapt") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
@@ -10,4 +10,5 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
+    id("androidx.room") version "2.8.3"
 }
