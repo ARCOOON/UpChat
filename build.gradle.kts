a36bbaa0ed4e8d2cb1a9b2fcf826adf14e7160ce
Arcooon <leonv11@gmx.net>
2025-11-23 02:50:12 +0100

    chore(deps): bump androidx.room from 2.8.3 to 2.8.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     id("com.google.firebase.crashlytics") version "3.0.6" apply false
-    id("androidx.room") version "2.8.3"
+    id("androidx.room") version "2.8.4"
 }
